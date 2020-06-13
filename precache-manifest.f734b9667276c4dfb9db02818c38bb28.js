bdea89280ca2bdb97946d68454cc5ab828eacb2b
GorAraqelyan <50023373+GorAraqelyan@users.noreply.github.com>
2020-06-13 16:48:00 +0400

    Updates

---
+++ precache-manifest.f734b9667276c4dfb9db02818c38bb28.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8688ba2c00589054027cf1506bbe587a",
+    "url": "/myApp-social-network/index.html"
+  },
+  {
+    "revision": "f62f4975a3ce13eea10f",
+    "url": "/myApp-social-network/static/css/3.50779102.chunk.css"
+  },
+  {
+    "revision": "4765a832e95f6a630569",
+    "url": "/myApp-social-network/static/css/4.674e462f.chunk.css"
+  },
+  {
+    "revision": "b61163ff6a24eea87825",
+    "url": "/myApp-social-network/static/css/main.370dda9a.chunk.css"
+  },
+  {
+    "revision": "f5d8abbac221bce31293",
+    "url": "/myApp-social-network/static/js/2.1c203ea7.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/myApp-social-network/static/js/2.1c203ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62f4975a3ce13eea10f",
+    "url": "/myApp-social-network/static/js/3.2ae9975b.chunk.js"
+  },
+  {
+    "revision": "4765a832e95f6a630569",
+    "url": "/myApp-social-network/static/js/4.927c3fed.chunk.js"
+  },
+  {
+    "revision": "b61163ff6a24eea87825",
+    "url": "/myApp-social-network/static/js/main.09fafd62.chunk.js"
+  },
+  {
+    "revision": "32e2b0e7aaa05e63966f",
+    "url": "/myApp-social-network/static/js/runtime-main.850c4125.js"
+  },
+  {
+    "revision": "55b7b1d414ea6d2094b85e3a834e32b8",
+    "url": "/myApp-social-network/static/media/MontserratAlternates-Medium.55b7b1d4.ttf"
+  },
+  {
+    "revision": "1180414073e7c45a7da2563c65a1cc95",
+    "url": "/myApp-social-network/static/media/load.11804140.gif"
+  },
+  {
+    "revision": "def11ce0fa024a22557665708d3ea00c",
+    "url": "/myApp-social-network/static/media/myApp.def11ce0.png"
+  }
+]);
